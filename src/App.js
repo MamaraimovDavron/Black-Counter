48d44791822d9347e4ba9ea9dc2e4d4be9d078ff
Davron <davronmamaraimov1996@gmail.com>
2022-09-22 20:38:12 +0500

    all deleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = ({
+            count: 0,
+        })
+    }
+
+    addOne = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    resetOne = () => {
+        this.setState({count: this.state.count = 0})
+    }
+
+    deleteOne = () => {
+        this.setState({count: this.state.count - 1})
+    }
+
+    render(){
+        return(
+            <div className='container'>
+            <h1>Counter</h1>
+                <div className='box--container'>
+                    <span>{this.state.count}</span> 
+                    <button onClick={this.addOne} id='plus'>+</button>
+                    <button onClick={this.resetOne} id='reset'>&#x21bb;</button>
+                    <button onClick={this.deleteOne} id='minus'>-</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
